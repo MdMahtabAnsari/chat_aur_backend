b5f6b83ad2833ab38ced0f0b9867a84d52456111
MdMahtabAnsari <68557208+MdMahtabAnsari@users.noreply.github.com>
2023-12-10 17:18:53 +0530

    databse setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
-const connect = async () => {
+const connectDB = async () => {
     try {
     const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
-    console.log("Connected to DB");
+    console.log(`Connected to DB ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log(`MongoBD connection ERROR ${error}`);
     process.exit(1);
   }
 }
+export default connectDB;
